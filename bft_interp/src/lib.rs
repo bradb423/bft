c3573729d937ee7ed91c9e47b7972d3d4c4ded8c
Bradley Burns <bradleyburns00@gmail.com>
2022-07-25 18:41:04 +0100

    Create a strucutre for the interpreter
    - Added a struch `VirtualMachine` which will represent the interpretation of
      the Brainfuck program
    - Added an implementation of the above to create a new `VirtualMachine` struct
    - Changed some formatting in `main.rs`

--- bft_interp/src/lib.rs
+++ bft_interp/src/lib.rs
@@ -1,3 +1,31 @@
+/// A "Virtual Machine" for the Brainfuck program to be interpreted in.
+/// This struct consists of a Tape (an array of numbers) and a Head (a pointer
+/// to the a position in the array).
+///
+/// Classical Brainfuck programs have byte size numbers (0 to 255) and the size
+/// of the array is by default set at 30,000.
+#[derive(Debug)]
+pub struct VirtualMachine<T> {
+    tape: Vec<T>,
+    tape_length: usize,
+    tape_head: usize,
+    growable: bool,
+}
+
+impl<T> VirtualMachine<T> {
+    pub fn new(mut tape_length: usize, tape_head: usize, growable: bool) -> Self {
+        if tape_length == 0 {
+            tape_length = 30000
+        }
+        Self {
+            tape: Vec::with_capacity(tape_length),
+            tape_length,
+            tape_head,
+            growable,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::env::args;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let filename = args()
-    .nth(1)
-    .ok_or("Please give a valid filename")?;
+    let filename = args().nth(1).ok_or("Please give a valid filename")?;
     let bf_program = BfProgram::from_file(filename)?;
     Ok(())
 }
