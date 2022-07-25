22ef2e31014db95c8aff21d62a0e3388ea1ad59a
Bradley Burns <bradleyburns00@gmail.com>
2022-07-25 15:10:14 +0100

    Adding the useful types
    - Added an enum for the raw instructions that are valid Brianfuck commands.
    - Added an implementation block, allowing from the version of specific
      characters to these instructions.
    - Added a struct to represent the Brainfuck Program.
    - Added an implementation block to take a given file, and the struct above.
    - Added some accessor methods for the data within.

--- bft_types/src/lib.rs
+++ bft_types/src/lib.rs
@@ -1,3 +1,90 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+
+/// Raw Brainfuck Instruction
+#[derive(Debug)]
+pub enum Instruction {
+    /// Represents the `>` character
+    IncrementPointer,
+    /// Represents the `<` character
+    DecrementPointer,
+    /// Represents the `+` character
+    IncrementByte,
+    /// Represents the `-` character
+    DecrementByte,
+    /// Represents the `.` character
+    OutputByte,
+    /// Represents the `,` character
+    InputByte,
+    /// Represents the `[` character
+    StartLoop,
+    /// Represents the `]` character
+    EndLoop,
+}
+
+impl Instruction {
+    /// Converts a character in a Brainfuck program into a raw instruction.
+    /// Returns None if the character is not a valid Brainfuck instruction.
+    fn char_to_instruction(c: char) -> Option<Instruction> {
+        match c {
+            '>' => Some(Instruction::IncrementPointer),
+            '<' => Some(Instruction::DecrementPointer),
+            '+' => Some(Instruction::IncrementByte),
+            '-' => Some(Instruction::DecrementByte),
+            '.' => Some(Instruction::OutputByte),
+            ',' => Some(Instruction::InputByte),
+            '[' => Some(Instruction::StartLoop),
+            ']' => Some(Instruction::EndLoop),
+            _ => None,
+        }
+    }
+}
+
+/// A struct representing a Brainfuck program, with the set of instructions, and
+/// the filename of the program.
+#[derive(Debug)]
+pub struct BfProgram {
+    instructions: Vec<Instruction>,
+    filename: PathBuf,
+}
+
+impl BfProgram {
+    /// Creates a new Brainfuck program, from a given string of contents and a
+    /// filename.
+    pub fn new<P>(contents: String, filename: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        let instructions = contents
+            .chars()
+            .filter_map(Instruction::char_to_instruction)
+            .collect();
+        Self {
+            instructions,
+            filename: filename.as_ref().to_path_buf(),
+        }
+    }
+
+    /// Reads directly from a file, to produce a Brainfuck program.
+    pub fn from_file<P>(filename: P) -> Result<BfProgram, Box<dyn Error>>
+    where
+        P: AsRef<Path>,
+    {
+        let contents = fs::read_to_string(&filename)?;
+        Ok(BfProgram::new(contents, filename))
+    }
+
+    pub fn instructions(&self) -> &[Instruction] {
+        &self.instructions
+    }
+
+    pub fn filename(&self) -> &Path {
+        &self.filename
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use bft_types::BfProgram;
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = args()
+    .nth(1)
+    .ok_or("Please give a valid filename")?;
+    let bf_program = BfProgram::from_file(filename)?;
+    Ok(())
 }
