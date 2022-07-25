c74cd3b2ddc25111b0b91f9709edb37c550756dd
Bradley Burns <bradleyburns00@gmail.com>
2022-07-25 21:27:48 +0100

    Add CLI parser for the program

---
+++ src/cli.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+/// A Brainfuck Interpreter, written in Rust. This is the final homework of
+/// Daniel Silverstone's rust workshop at Codethink.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub(crate) struct Args {
+    /// The filename of the program to interpret.
+    #[clap(name = "filename", short, long, parse(from_os_str))]
+    pub(crate) filename: PathBuf,
+
+    /// The number of cells in the tape of the Virtual Machine.
+    #[clap(name = "cell", short, long, value_parser, default_value_t = 30000)]
+    pub(crate) cells: usize,
+
+    /// Whether or not the tape of the Virtual Machine can be extensible.
+    #[clap(name = "extensible", short, long, value_parser, default_value_t = false)]
+    pub(crate) extensible: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use bft_interp::VirtualMachine;
 use bft_types::BfProgram;
-use std::env::args;
 use std::error::Error;
+use clap::Parser;
+
+mod cli;
 
 /// Main entry point of the program
 fn main() -> Result<(), Box<dyn Error>> {
-    let filename = args().nth(1).ok_or("Please give a valid filename")?;
-    let bf_program = BfProgram::from_file(filename)?;
-    let interpreter = VirtualMachine::<u8>::new(0, false);
+    let arguments = cli::Args::parse();
+    let bf_program = BfProgram::from_file(arguments.filename)?;
+    let interpreter = VirtualMachine::<u8>::new(arguments.cells, false);
     interpreter.interpret(&bf_program);
     Ok(())
 }
