06d78eae2294f4d8d2255f4b3fe13e1e52201a09
Bradley Burns <bradley.burns@codethink.co.uk>
2022-09-28 19:56:41 +0100

    Upgrade to clap 4.0.0

--- src/cli.rs
+++ src/cli.rs
@@ -5,22 +5,17 @@ use std::path::PathBuf;
 
 /// A Brainfuck Interpreter, written in Rust.
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub(crate) struct Args {
     /// The filename of the program to interpret.
     pub(crate) filename: PathBuf,
 
     /// The number of cells in the tape of the Virtual Machine.
-    #[clap(name = "cell", short, long, value_parser, default_value_t = 30000)]
+    // #[clap(name = "cell", short, long, value_parser, default_value_t = 30000)]
+    #[arg(short, long, default_value_t = 30_000)]
     pub(crate) cells: usize,
 
     /// Whether or not the tape of the Virtual Machine can be extensible.
-    #[clap(
-        name = "extensible",
-        short,
-        long,
-        value_parser,
-        default_value_t = false
-    )]
+    #[arg(short, long, default_value_t = false)]
     pub(crate) extensible: bool,
 }
