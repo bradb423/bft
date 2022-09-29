79e9b57050ecf8665bd01418d9d5298e42438ec9
Bradley Burns <bradley.burns@codethink.co.uk>
2022-09-29 08:48:48 +0100

    Update the display implementation for the raw instructions
    - Made the descriptions of the different instructions more clear in the display
      method.

--- bft_types/src/ops.rs
+++ bft_types/src/ops.rs
@@ -47,14 +47,14 @@ impl Operation {
 impl fmt::Display for Operation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Operation::IncrementPointer => write!(f, ">"),
-            Operation::DecrementPointer => write!(f, "<"),
-            Operation::IncrementByte => write!(f, "+"),
-            Operation::DecrementByte => write!(f, "-"),
-            Operation::OutputByte => write!(f, "."),
-            Operation::InputByte => write!(f, ","),
-            Operation::StartLoop => write!(f, "["),
-            Operation::EndLoop => write!(f, "]"),
+            Operation::IncrementPointer => write!(f, "> : moves the data pointer to the right by one cell."),
+            Operation::DecrementPointer => write!(f, "< : moves the data pointer to the left by one cell."),
+            Operation::IncrementByte => write!(f, "+ : increases the value stored at the current cell by 1."),
+            Operation::DecrementByte => write!(f, "- : decreases the value stored at the current cell by 1."),
+            Operation::OutputByte => write!(f, ". : Outputs the byte at the current data pointer."),
+            Operation::InputByte => write!(f, ", : Accepts a byte of input, and stores the value at the current data pointer."),
+            Operation::StartLoop => write!(f, "[ : Starts a loop."),
+            Operation::EndLoop => write!(f, "] : Ends a loop."),
         }
     }
 }
@@ -66,48 +66,48 @@ mod tests {
     #[test]
     fn test_display_increment_pointer() {
         let increment_pointer: Operation = Operation::IncrementPointer;
-        assert_eq!(increment_pointer.to_string(), ">");
+        assert_eq!(increment_pointer.to_string(), "> : moves the data pointer to the right by one cell.");
     }
 
     #[test]
     fn test_display_decrement_pointer() {
         let decrement_pointer: Operation = Operation::DecrementPointer;
-        assert_eq!(decrement_pointer.to_string(), "<");
+        assert_eq!(decrement_pointer.to_string(), "< : moves the data pointer to the left by one cell.");
     }
 
     #[test]
     fn test_display_increment_byte() {
         let increment_byte: Operation = Operation::IncrementByte;
-        assert_eq!(increment_byte.to_string(), "+");
+        assert_eq!(increment_byte.to_string(), "+ : increases the value stored at the current cell by 1.");
     }
 
     #[test]
     fn test_display_decrement_byte() {
         let decrement_byte: Operation = Operation::DecrementByte;
-        assert_eq!(decrement_byte.to_string(), "-");
+        assert_eq!(decrement_byte.to_string(), "- : decreases the value stored at the current cell by 1.");
     }
 
     #[test]
     fn test_display_output_byte() {
         let output_byte: Operation = Operation::OutputByte;
-        assert_eq!(output_byte.to_string(), ".");
+        assert_eq!(output_byte.to_string(), ". : Outputs the byte at the current data pointer.");
     }
 
     #[test]
     fn test_display_input_byte() {
         let input_byte: Operation = Operation::InputByte;
-        assert_eq!(input_byte.to_string(), ",");
+        assert_eq!(input_byte.to_string(), ", : Accepts a byte of input, and stores the value at the current data pointer.");
     }
 
     #[test]
     fn test_display_start_loop() {
         let start_loop: Operation = Operation::StartLoop;
-        assert_eq!(start_loop.to_string(), "[");
+        assert_eq!(start_loop.to_string(), "[ : Starts a loop.");
     }
 
     #[test]
     fn test_display_end_loop() {
         let end_loop: Operation = Operation::EndLoop;
-        assert_eq!(end_loop.to_string(), "]");
+        assert_eq!(end_loop.to_string(), "] : Ends a loop.");
     }
 }
